5248b344a23779d2c786c990885e0f85264d1238
makek <awfafmaf@gmail.com>
2021-04-29 20:53:19 +0400

    '1.0'

---
+++ newes/metascore_parse.py
@@ -0,0 +1,29 @@
+
+
+def Parse(URL):
+    import requests
+    from bs4 import BeautifulSoup
+    HEADERS = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
+        'accept': '*/*'}
+    html = requests.get(URL, headers=HEADERS)
+    if html.status_code == 200:
+        html = html.text
+
+        soup = BeautifulSoup(html, 'html.parser')
+        items = soup.find_all('div', class_='metascore_w xlarge game positive')
+        if items == []:
+            items = soup.find_all('div', class_='metascore_w xlarge game mixed')
+        if items == []:
+            items = soup.find_all('div', class_='metascore_w xlarge game negative')
+        items = str(items)
+        items = items.replace(items[0:items.find('span')+28],'').replace(items[items.find('</'):],'')
+
+        return items
+
+    else:
+        print('Error')
+        return False
+
+
+
